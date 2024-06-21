c5a19526cbe2e32ca7e48c445f12e4db498cd9fe
Ulrich Feindt <ufeindt@gmail.com>
2024-06-21 19:27:28 +0200

    add zoxide

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     set fish_greeting
     fish_config theme choose "Catppuccin Mocha"
     starship init fish | source
+    zoxide init fish | source
     fish_ssh_agent
 end
