d2a2fa70225fff21be6040f55d29ad508c7c8beb
Ulrich Feindt <ufeindt@gmail.com>
2024-07-15 14:22:41 +0200

    run keychain on all interactive shells

--- config/fish/conf.d/keychain.fish
+++ config/fish/conf.d/keychain.fish
@@ -1,5 +1,4 @@
-if status is-login
-    and status is-interactive
+if status is-interactive
     # To add a key, set -Ua SSH_KEYS_TO_AUTOLOAD keypath
     # To remove a key, set -U --erase SSH_KEYS_TO_AUTOLOAD[index_of_key]
     SHELL=/usr/bin/fish keychain --quick --quiet --eval $SSH_KEYS_TO_AUTOLOAD | source
