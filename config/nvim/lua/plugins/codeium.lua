8336bc8901b7ed342d38b1c9ed9b142e5e77f663
Ulrich Feindt <ufeindt@gmail.com>
2024-10-08 09:28:54 +0200

    Remap Codeium keybinds

--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,4 +1,8 @@
 return {
   'Exafunction/codeium.vim',
-  event = 'BufEnter'
+  event = 'BufEnter',
+  config = function()
+    vim.g.codeium_no_map_tab = 1
+    vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+  end
 }
