ebed3e443d7dd604ed3182505344aef3533a2787
Ulrich Feindt <ulrich.feindt@betao.se>
2025-10-16 15:45:43 +0200

    Update .chezmoiignore Add .config/fish/functions/pdc.fish

---
+++ private_dot_config/fish/functions/pdc.fish
@@ -0,0 +1,4 @@
+function pdc --wraps=/home/ulrich/Workspace/portail/docker-compose.sh --description 'alias pdc /home/ulrich/Workspace/portail/docker-compose.sh'
+  /home/ulrich/Workspace/portail/docker-compose.sh $argv
+        
+end
